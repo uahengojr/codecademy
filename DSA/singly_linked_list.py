f19f133fa9382abc8f43497c834699ae304483e3
uahengojr <uahengo@me.com>
2023-12-26 22:44:26 +0200

    Add constructor and getter methods

--- DSA/singly_linked_list.py
+++ DSA/singly_linked_list.py
@@ -1,4 +1,11 @@
-import node
+import node as Node
 
 class SinglyLinkedList:
-    pass
+
+    "A linked list class"
+
+    def __init__(self, data) -> None:
+        self.head_node = Node(data)
+
+    def get_head(self):
+        return self.head_node
