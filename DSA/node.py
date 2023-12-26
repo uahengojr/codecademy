95b317fba56f914e69801f328280398b0733d6e5
uahengojr <uahengo@me.com>
2023-12-26 22:43:57 +0200

    Re-arrange node class methods

--- DSA/node.py
+++ DSA/node.py
@@ -1,6 +1,6 @@
 class Node:
     """ A singly linked node class """
-
+    
     def __init__(self, data, next=None):
         self.data = data
         self.next = next
@@ -8,8 +8,10 @@ class Node:
     def get_value(self):
         return self.data
     
+    def get_next(self):
+        return self.next
+    
     def set_next(self, next):
         self.next = next
     
-    def get_next(self):
-        return self.next
+    
